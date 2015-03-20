19549461acae40cbf32e13f72e97cfd2d91b4482
Fernando Moreno <fernando@users.noreply.github.com>
2015-03-20 08:13:23 -0500

    ShaderSource builder header file added

---
+++ include/post3D/ShaderSourceBuilder.h
@@ -0,0 +1,31 @@
+﻿#ifndef SHADERSOURCEBUILDER_H
+#define SHADERSOURCEBUILDER_H
+
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+namespace material{
+class ShaderSourceBuilder{
+public:
+	string version;
+	unordered_map<string,string> attribute;
+	unordered_map<string,string> uniform;
+	unordered_map<string,string> output;
+	unordered_map<string,string> vertexChunk;
+	unordered_map<string,string> fragmentChunk;
+	unordered_map<string,string> vertexParams;
+	unordered_map<string,string> fragmentParams;
+	unordered_map<string,string> helpers;
+	ShaderSourceBuilder();
+	ShaderSourceBuilder(string version);
+	ShaderSourceBuilder(const ShaderSourceBuilder& ssb);
+	ShaderSourceBuilder(ShaderSourceBuilder&& ssb);
+	ShaderSourceBuilder& operator=(const ShaderSourceBuilder& ssb);
+	ShaderSourceBuilder& operator=(ShaderSourceBuilder&& ssb);
+	~ShaderSourceBuilder();
+};
+}
+
+#endif
