3a3bb15625cc976f34e502d6fa0ff057895a2fa0
Fernando Moreno <fernando@users.noreply.github.com>
2015-03-22 19:42:08 -0500

    ShaderBuilderSource has shader strings

--- include/post3D/ShaderSourceBuilder.h
+++ include/post3D/ShaderSourceBuilder.h
@@ -9,22 +9,21 @@ using namespace std;
 namespace material{
 class ShaderSourceBuilder{
 public:
-	string version;
-	unordered_map<string,string> attribute;
-	unordered_map<string,string> uniform;
-	unordered_map<string,string> output;
-	unordered_map<string,string> vertexChunk;
-	unordered_map<string,string> fragmentChunk;
-	unordered_map<string,string> vertexParams;
-	unordered_map<string,string> fragmentParams;
-	unordered_map<string,string> helpers;
-	ShaderSourceBuilder();
-	ShaderSourceBuilder(string version);
-	ShaderSourceBuilder(const ShaderSourceBuilder& ssb);
-	ShaderSourceBuilder(ShaderSourceBuilder&& ssb);
-	ShaderSourceBuilder& operator=(const ShaderSourceBuilder& ssb);
-	ShaderSourceBuilder& operator=(ShaderSourceBuilder&& ssb);
-	~ShaderSourceBuilder();
+  string version;
+  unordered_map<string,string> attribute;
+  unordered_map<string,string> uniform;
+  unordered_map<string,string> output;
+  unordered_map<string,string> vertexChunk;
+  unordered_map<string,string> fragmentChunk;
+  unordered_map<string,string> vertexParams;
+  unordered_map<string,string> fragmentParams;
+  unordered_map<string,string> helpers;
+  ShaderSourceBuilder();
+  ShaderSourceBuilder(const ShaderSourceBuilder& ssb)=default;
+  ShaderSourceBuilder(ShaderSourceBuilder&& ssb)=default;
+  ShaderSourceBuilder& operator=(const ShaderSourceBuilder& ssb);=default
+  ShaderSourceBuilder& operator=(ShaderSourceBuilder&& ssb)=default;
+  ~ShaderSourceBuilder()=default;
 };
 }
 
