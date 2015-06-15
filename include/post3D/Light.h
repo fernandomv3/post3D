58ca745cf26a805fb45767a245fb3ff9e4ed6d73
fernandomv3 <fernandomv3@gmail.com>
2015-06-15 14:54:12 -0500

    Added a mesh attribute to lights for deferred shading light pass

--- include/post3D/Light.h
+++ include/post3D/Light.h
@@ -1,6 +1,7 @@
 ﻿#ifndef LIGHT_H
 #define LIGHT_H
 #include "post3D/Object3D.h"
+#include "post3D/Mesh.h"
 #include "post3D/Color.h"
 #include <memory>
 
@@ -9,18 +10,21 @@ using namespace math3D;
 using namespace object3D;
 
 namespace light{
-	class Light : public Object3D {
-	private:
-		shared_ptr<Color> color;
-	public:
-		Light();
-		Light(const Light& light);
-		Light(Light&& light);
-		Light& operator=(const Light& light);
-		Light& operator=(Light&& light);
-		virtual ~Light()=default;
-		shared_ptr<Color> getColor() const;
-		Light& setColor(shared_ptr<Color> color);
-	};
+class Light : public Object3D {
+private:
+	shared_ptr<Color> color;
+	shared_ptr<Mesh> mesh;
+public:
+	Light();
+	Light(const Light& light);
+	Light(Light&& light);
+	Light& operator=(const Light& light);
+	Light& operator=(Light&& light);
+	virtual ~Light()=default;
+	shared_ptr<Color> getColor() const;
+	Light& setColor(shared_ptr<Color> color);
+	shared_ptr<Mesh> getMesh() const;
+	Light& setMesh(shared_ptr<Mesh> mesh);
+};
 }
 #endif
--- src/Light.cpp
+++ src/Light.cpp
@@ -1,38 +1,49 @@
 ﻿#include "post3D/Light.h"
 using namespace std;
 using namespace math3D;
+
 namespace light{
-		Light::Light():Object3D(){
-			this->color = shared_ptr<Color>(new Color());
-		}
 
-		Light::Light(const Light& light):Object3D(light){
-			this->color = light.color;
-		}
+Light::Light():Object3D(){
+	this->color = shared_ptr<Color>(new Color());
+}
 
-		Light::Light(Light&& light):Object3D(light){
-			this->color = move(light.color);
-		}
+Light::Light(const Light& light):Object3D(light){
+	this->color = light.color;
+}
 
-		Light& Light::operator=(const Light& light){
-			Object3D::operator=(light);
-			this->color = light.color;
-			return *this;
-		}
+Light::Light(Light&& light):Object3D(light){
+	this->color = move(light.color);
+}
 
-		Light& Light::operator=(Light&& light){
-			Object3D::operator=(move(light));
-			this->color = move(light.color);
-			return *this;
-		}
+Light& Light::operator=(const Light& light){
+	Object3D::operator=(light);
+	this->color = light.color;
+	return *this;
+}
 
-		shared_ptr<Color> Light::getColor()const{
-			return this->color;
-		}
+Light& Light::operator=(Light&& light){
+	Object3D::operator=(move(light));
+	this->color = move(light.color);
+	return *this;
+}
 
-		Light& Light::setColor(shared_ptr<Color> color){
-			this->color = color;
-			return *this;
-		}
+shared_ptr<Color> Light::getColor()const{
+	return this->color;
+}
+
+Light& Light::setColor(shared_ptr<Color> color){
+	this->color = color;
+	return *this;
+}
+
+shared_ptr<Mesh> Light::getMesh() const{
+	return this->mesh;
+}
+
+Light& Light::setMesh(shared_ptr<Mesh> mesh){
+	this->mesh = mesh;
+	return *this;
+}
 
 }
