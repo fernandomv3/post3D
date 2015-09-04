9bb764888983d2fac2e6af8405eb3137fc424e2c
fernandomv3 <fernandomv3@gmail.com>
2015-09-04 12:56:06 -0500

    Euler class indentation and namespacing fix

--- include/post3D/Euler.h
+++ include/post3D/Euler.h
@@ -2,39 +2,38 @@
 #define EULER_H
 #include <memory>
 #include <string>
-using namespace std;
 namespace math3D{
-	class Quaternion;
+class Quaternion;
 
-	class Euler{
-	private:
-		float x;
-		float y;
-		float z;
-		string order;
-		weak_ptr<Quaternion> quaternion;
+class Euler{
+private:
+  float x;
+  float y;
+  float z;
+  std::string order;
+  std::weak_ptr<Quaternion> quaternion;
 
-	public:
-		Euler();
-		Euler(float x, float y, float z, const string order);
-		Euler(float x, float y, float z);
-		Euler(const Euler& euler);
-		Euler(Euler&& euler);
-		Euler& operator=(const Euler& euler);
-		Euler& operator=(Euler&& euler);
-		~Euler()=default;
-		string getOrder()const;
-		float getX()const;
-		Euler& setX(float x);
-		float getY()const;
-		Euler& setY(float y);
-		float getZ()const;
-		Euler& setZ(float z);
-		Euler& setFromQuaternion(Quaternion& q, const string order, bool update = true);
-		Euler& updateQuaternion();
-		Euler& setQuaternion(shared_ptr<Quaternion> q);
-		shared_ptr<Quaternion> getQuaternion()const;
-	};
-	float clamp(float num);
+public:
+  Euler();
+  Euler(float x, float y, float z, const std::string order);
+  Euler(float x, float y, float z);
+  Euler(const Euler& euler);
+  Euler(Euler&& euler);
+  Euler& operator=(const Euler& euler);
+  Euler& operator=(Euler&& euler);
+  ~Euler()=default;
+  std::string getOrder()const;
+  float getX()const;
+  Euler& setX(float x);
+  float getY()const;
+  Euler& setY(float y);
+  float getZ()const;
+  Euler& setZ(float z);
+  Euler& setFromQuaternion(Quaternion& q, const std::string order, bool update = true);
+  Euler& updateQuaternion();
+  Euler& setQuaternion(std::shared_ptr<Quaternion> q);
+  std::shared_ptr<Quaternion> getQuaternion()const;
+};
+float clamp(float num);
 }
 #endif
--- src/Euler.cpp
+++ src/Euler.cpp
@@ -4,159 +4,159 @@
 #include "post3D/Quaternion.h"
 namespace math3D{
 
-		Euler::Euler(){
-			this->x = 0.0f;
-			this->y = 0.0f;
-			this->z = 0.0f;
-			this->order = string("XYZ",3);
-		}
+Euler::Euler(){
+  this->x = 0.0f;
+  this->y = 0.0f;
+  this->z = 0.0f;
+  this->order = std::string("XYZ",3);
+}
 
-		Euler::Euler(float x, float y, float z, const string order){
-			this->x = x;
-			this->y = y;
-			this->z = z;
-			this->order = order;
-		}
+Euler::Euler(float x, float y, float z, const std::string order){
+  this->x = x;
+  this->y = y;
+  this->z = z;
+  this->order = order;
+}
 
-		Euler::Euler(float x, float y, float z){
-			this->x = x;
-			this->y = y;
-			this->z = z;
-			this->order = string("XYZ",3);
-		}
+Euler::Euler(float x, float y, float z){
+  this->x = x;
+  this->y = y;
+  this->z = z;
+  this->order = std::string("XYZ",3);
+}
 
-		Euler::Euler(const Euler& euler){
-			this->x =  euler.x;
-			this->y = euler.y;
-			this->z = euler.z;
-			this->order = euler.order;
-			this->quaternion = euler.quaternion;
-		}
+Euler::Euler(const Euler& euler){
+  this->x =  euler.x;
+  this->y = euler.y;
+  this->z = euler.z;
+  this->order = euler.order;
+  this->quaternion = euler.quaternion;
+}
 
-		Euler::Euler(Euler&& euler){
-			this->x =  euler.x;
-			this->y = euler.y;
-			this->z = euler.z;
-			this->order = euler.order;
-			this->quaternion = move(euler.quaternion);
-		}
+Euler::Euler(Euler&& euler){
+  this->x =  euler.x;
+  this->y = euler.y;
+  this->z = euler.z;
+  this->order = euler.order;
+  this->quaternion = std::move(euler.quaternion);
+}
 
-		Euler& Euler::operator=(const Euler& euler){
-			this->x =  euler.x;
-			this->y = euler.y;
-			this->z = euler.z;
-			this->order = euler.order;
-			this->quaternion = euler.quaternion;
-			return *this;
-		}
+Euler& Euler::operator=(const Euler& euler){
+  this->x =  euler.x;
+  this->y = euler.y;
+  this->z = euler.z;
+  this->order = euler.order;
+  this->quaternion = euler.quaternion;
+  return *this;
+}
 
-		Euler& Euler::operator=(Euler&& euler){
-			this->x =  euler.x;
-			this->y = euler.y;
-			this->z = euler.z;
-			this->order = euler.order;
-			this->quaternion = move(euler.quaternion);
-			return *this;
-		}
+Euler& Euler::operator=(Euler&& euler){
+  this->x =  euler.x;
+  this->y = euler.y;
+  this->z = euler.z;
+  this->order = euler.order;
+  this->quaternion = std::move(euler.quaternion);
+  return *this;
+}
 
-		string Euler::getOrder()const{
-			return this->order;
-		}
+std::string Euler::getOrder()const{
+  return this->order;
+}
 
-		float Euler::getX()const{
-			return this->x;
-		}
+float Euler::getX()const{
+  return this->x;
+}
 
-		Euler& Euler::setX(float x){
-			this->x = x;
-			this->updateQuaternion();
-			return *this;
-		}
+Euler& Euler::setX(float x){
+  this->x = x;
+  this->updateQuaternion();
+  return *this;
+}
 
-		float Euler::getY()const{
-			return this->y;
-		}
+float Euler::getY()const{
+  return this->y;
+}
 
-		Euler& Euler::setY(float y){
-			this->y = y;
-			this->updateQuaternion();
-			return *this;
-		}
+Euler& Euler::setY(float y){
+  this->y = y;
+  this->updateQuaternion();
+  return *this;
+}
 
-		float Euler::getZ()const{
-			return this->z;
-		}
+float Euler::getZ()const{
+  return this->z;
+}
 
-		Euler& Euler::setZ(float z){
-			this->z = z;
-			this->updateQuaternion();
-			return *this;
-		}
+Euler& Euler::setZ(float z){
+  this->z = z;
+  this->updateQuaternion();
+  return *this;
+}
 
-		Euler& Euler::setFromQuaternion(Quaternion& q, const string order, bool update){
-			float x2 = q.getX() * q.getX(), 
-			      y2 = q.getY() * q.getY(), 
-			      z2 = q.getZ() * q.getZ(), 
-			      w2 = q.getW() * q.getW();
+Euler& Euler::setFromQuaternion(Quaternion& q, const std::string order, bool update){
+  float x2 = q.getX() * q.getX(), 
+        y2 = q.getY() * q.getY(), 
+        z2 = q.getZ() * q.getZ(), 
+        w2 = q.getW() * q.getW();
 
-			if (order == string("XYZ")){
-				this->x = atan2( 2 * ( q.getX() * q.getW() - q.getY() * q.getZ() ), ( w2 - x2 - y2 + z2 ) );
-		        this->y = asin(  clamp( 2 * ( q.getX() * q.getZ() + q.getY() * q.getW() ) ) );
-		        this->z = atan2( 2 * ( q.getZ() * q.getW() - q.getX() * q.getY() ), ( w2 + x2 - y2 - z2 ) );
+  if (order == std::string("XYZ")){
+    this->x = atan2( 2 * ( q.getX() * q.getW() - q.getY() * q.getZ() ), ( w2 - x2 - y2 + z2 ) );
+    this->y = asin(  clamp( 2 * ( q.getX() * q.getZ() + q.getY() * q.getW() ) ) );
+    this->z = atan2( 2 * ( q.getZ() * q.getW() - q.getX() * q.getY() ), ( w2 + x2 - y2 - z2 ) );
 
-			}
-			else if (order == string("YXZ")){
-				this->x = asin(  clamp( 2 * ( q.getX() * q.getW() - q.getY() * q.getZ() ) ) );
-		        this->y = atan2( 2 * ( q.getX() * q.getZ() + q.getY() * q.getW() ), ( w2 - x2 - y2 + z2 ) );
-		        this->z = atan2( 2 * ( q.getX() * q.getY() + q.getZ() * q.getW() ), ( w2 - x2 + y2 - z2 ) );
-			}
-			else if (order == string("ZXY")){
-				this->x = asin(  clamp( 2 * ( q.getX() * q.getW() + q.getY() * q.getZ() ) ) );
-				this->y = atan2( 2 * ( q.getY() * q.getW() - q.getZ() * q.getX() ), ( w2 - x2 - y2 + z2 ) );
-				this->z = atan2( 2 * ( q.getZ() * q.getW() - q.getX() * q.getY() ), ( w2 - x2 + y2 - z2 ) );
-			}
-			else if (order == string("ZYX")){
-				this->x = atan2( 2 * ( q.getX() * q.getW() + q.getZ() * q.getY() ), ( w2 - x2 - y2 + z2 ) );
-		        this->y = asin(  clamp( 2 * ( q.getY() * q.getW() - q.getX() * q.getZ() ) ) );
-		        this->z = atan2( 2 * ( q.getX() * q.getY() + q.getZ() * q.getW() ), ( w2 + x2 - y2 - z2 ) );
-			}
-			else if (order == string("YZX")){
-				this->x = atan2( 2 * ( q.getX() * q.getW() - q.getZ() * q.getY() ), ( w2 - x2 + y2 - z2 ) );
-		        this->y = atan2( 2 * ( q.getY() * q.getW() - q.getX() * q.getZ() ), ( w2 + x2 - y2 - z2 ) );
-		        this->z = asin(  clamp( 2 * ( q.getX() * q.getY() + q.getZ() * q.getW() ) ) );
-			}
-			else if (order == string("XZY")){
-				this->x = atan2( 2 * ( q.getX() * q.getW() + q.getY() * q.getZ() ), ( w2 - x2 + y2 - z2 ) );
-		        this->y = atan2( 2 * ( q.getX() * q.getZ() + q.getY() * q.getW() ), ( w2 + x2 - y2 - z2 ) );
-		        this->z = asin(  clamp( 2 * ( q.getZ() * q.getW() - q.getX() * q.getY() ) ) );
-			}
+  }
+  else if (order == std::string("YXZ")){
+    this->x = asin(  clamp( 2 * ( q.getX() * q.getW() - q.getY() * q.getZ() ) ) );
+    this->y = atan2( 2 * ( q.getX() * q.getZ() + q.getY() * q.getW() ), ( w2 - x2 - y2 + z2 ) );
+    this->z = atan2( 2 * ( q.getX() * q.getY() + q.getZ() * q.getW() ), ( w2 - x2 + y2 - z2 ) );
+  }
+  else if (order == std::string("ZXY")){
+    this->x = asin(  clamp( 2 * ( q.getX() * q.getW() + q.getY() * q.getZ() ) ) );
+    this->y = atan2( 2 * ( q.getY() * q.getW() - q.getZ() * q.getX() ), ( w2 - x2 - y2 + z2 ) );
+    this->z = atan2( 2 * ( q.getZ() * q.getW() - q.getX() * q.getY() ), ( w2 - x2 + y2 - z2 ) );
+  }
+  else if (order == std::string("ZYX")){
+    this->x = atan2( 2 * ( q.getX() * q.getW() + q.getZ() * q.getY() ), ( w2 - x2 - y2 + z2 ) );
+    this->y = asin(  clamp( 2 * ( q.getY() * q.getW() - q.getX() * q.getZ() ) ) );
+    this->z = atan2( 2 * ( q.getX() * q.getY() + q.getZ() * q.getW() ), ( w2 + x2 - y2 - z2 ) );
+  }
+  else if (order == std::string("YZX")){
+    this->x = atan2( 2 * ( q.getX() * q.getW() - q.getZ() * q.getY() ), ( w2 - x2 + y2 - z2 ) );
+    this->y = atan2( 2 * ( q.getY() * q.getW() - q.getX() * q.getZ() ), ( w2 + x2 - y2 - z2 ) );
+    this->z = asin(  clamp( 2 * ( q.getX() * q.getY() + q.getZ() * q.getW() ) ) );
+  }
+  else if (order == std::string("XZY")){
+    this->x = atan2( 2 * ( q.getX() * q.getW() + q.getY() * q.getZ() ), ( w2 - x2 + y2 - z2 ) );
+    this->y = atan2( 2 * ( q.getX() * q.getZ() + q.getY() * q.getW() ), ( w2 + x2 - y2 - z2 ) );
+    this->z = asin(  clamp( 2 * ( q.getZ() * q.getW() - q.getX() * q.getY() ) ) );
+  }
 
 
-			this->order = order;
-			if(update){
-				this->updateQuaternion();
-			}
-			return *this;
-		}
+  this->order = order;
+  if(update){
+    this->updateQuaternion();
+  }
+  return *this;
+}
 
-		Euler& Euler::updateQuaternion(){
-			auto p = this->quaternion.lock();
-			if(p){
-				p->setFromEuler(*this , false);
-			}
-			return *this;
-		}
+Euler& Euler::updateQuaternion(){
+  auto p = this->quaternion.lock();
+  if(p){
+    p->setFromEuler(*this , false);
+  }
+  return *this;
+}
 
-		shared_ptr<Quaternion> Euler::getQuaternion()const{
-			return this->quaternion.lock();
-		}
+std::shared_ptr<Quaternion> Euler::getQuaternion()const{
+  return this->quaternion.lock();
+}
 
-		Euler& Euler::setQuaternion(shared_ptr<Quaternion> q){
-			this->quaternion = weak_ptr<Quaternion>{q};
-			return *this;
-		}
+Euler& Euler::setQuaternion(std::shared_ptr<Quaternion> q){
+  this->quaternion = std::weak_ptr<Quaternion>{q};
+  return *this;
+}
 
-		float clamp(float num){
-			return min(max(num,-1.0f),1.0f);
-		}
+float clamp(float num){
+  return min(max(num,-1.0f),1.0f);
+}
 }
