374f0f93926fa944bae44144bc38a8383b72b401
fernandomv3 <fernandomv3@gmail.com>
2015-09-04 15:17:38 -0500

    Scene class indentation and namespacing fix

--- include/post3D/Scene.h
+++ include/post3D/Scene.h
@@ -9,48 +9,47 @@
 #include <memory>
 #include <vector>
 
-using namespace std;
 using namespace light;
 using namespace object3D;
 
 namespace scene{
-	class Scene{
-	private:
-		shared_ptr<Camera> camera;
-		shared_ptr<Light> ambientLight;
-		vector<shared_ptr<Object3D>> objects;
-		vector<shared_ptr<Object3D>> addedObjects;
-		vector<shared_ptr<Object3D>> removedObjects;
-		vector<shared_ptr<DirectionalLight>> directionalLights;
-		vector<shared_ptr<PointLight>> pointLights;
-		float maxLightIntensity;
-		bool usesShadows;
-		bool PCFShadows;
-	public:
-		Scene();
-		Scene(const Scene& scene);
-		Scene(Scene&& scene);
-		Scene& operator=(const Scene& scene);
-		Scene& operator=(Scene&& scene);
-		~Scene()=default;
-		const vector<shared_ptr<Object3D>>& getObjects()const;
-		Scene& addObject(shared_ptr<Object3D> object);
-		Scene& removeObject(shared_ptr<Object3D> object);
-		shared_ptr<Light> getAmbientLight()const;
-		Scene& setAmbientLight(shared_ptr<Light> ambientLight);
-		shared_ptr<Camera> getCamera()const;
-		Scene& setCamera(shared_ptr<Camera> camera);
-		const vector<shared_ptr<DirectionalLight>>& getDirectionalLights()const;
-		const vector<shared_ptr<PointLight>>& getPointLights()const;
-		Scene& addPointLight(shared_ptr<PointLight> pointLight);
-		Scene& addDirectionalLight(shared_ptr<DirectionalLight> directionalLight);
-		float getMaxLightIntensity()const;
-		bool getUsesShadows()const;
-		bool getPCFShadows()const;
-		Scene& setMaxLightIntensity(float maxLightIntensity);
-		Scene& setUsesShadows(bool usesShadows);
-		Scene& setPCFShadows(bool PCFShadows);
-	};
+class Scene{
+private:
+  std::shared_ptr<Camera> camera;
+  std::shared_ptr<Light> ambientLight;
+  std::vector<std::shared_ptr<Object3D>> objects;
+  std::vector<std::shared_ptr<Object3D>> addedObjects;
+  std::vector<std::shared_ptr<Object3D>> removedObjects;
+  std::vector<std::shared_ptr<DirectionalLight>> directionalLights;
+  std::vector<std::shared_ptr<PointLight>> pointLights;
+  float maxLightIntensity;
+  bool usesShadows;
+  bool PCFShadows;
+public:
+  Scene();
+  Scene(const Scene& scene);
+  Scene(Scene&& scene);
+  Scene& operator=(const Scene& scene);
+  Scene& operator=(Scene&& scene);
+  ~Scene()=default;
+  const std::vector<std::shared_ptr<Object3D>>& getObjects()const;
+  Scene& addObject(std::shared_ptr<Object3D> object);
+  Scene& removeObject(std::shared_ptr<Object3D> object);
+  std::shared_ptr<Light> getAmbientLight()const;
+  Scene& setAmbientLight(std::shared_ptr<Light> ambientLight);
+  std::shared_ptr<Camera> getCamera()const;
+  Scene& setCamera(std::shared_ptr<Camera> camera);
+  const std::vector<std::shared_ptr<DirectionalLight>>& getDirectionalLights()const;
+  const std::vector<std::shared_ptr<PointLight>>& getPointLights()const;
+  Scene& addPointLight(std::shared_ptr<PointLight> pointLight);
+  Scene& addDirectionalLight(std::shared_ptr<DirectionalLight> directionalLight);
+  float getMaxLightIntensity()const;
+  bool getUsesShadows()const;
+  bool getPCFShadows()const;
+  Scene& setMaxLightIntensity(float maxLightIntensity);
+  Scene& setUsesShadows(bool usesShadows);
+  Scene& setPCFShadows(bool PCFShadows);
+};
 }
 
 #endif
--- src/Scene.cpp
+++ src/Scene.cpp
@@ -4,155 +4,155 @@
 
 namespace scene{
 
-	Scene::Scene(){
-		this->camera = shared_ptr<Camera>(new Camera());
-		shared_ptr<Mat4> matrix = shared_ptr<Mat4>(new Mat4(Mat4::perspectiveMatrix(30.0f, 1280.0f/720.0f, 0.1f, 100.0f)));
-		this->camera->setProjectionMatrix(matrix);
-		//position camera (this should not go here)
-		this->camera->getPosition()->setZ(12.0f);
-		this->ambientLight = shared_ptr<Light>(new Light());
-		this->ambientLight->getColor()->setRGBA(0.01f,0.01f,0.01f);
-		this->usesShadows = false;
-		this->PCFShadows = true;
-		this->maxLightIntensity = 0.0f;
+Scene::Scene(){
+  this->camera = std::shared_ptr<Camera>(new Camera());
+  std::shared_ptr<Mat4> matrix = std::shared_ptr<Mat4>(new Mat4(Mat4::perspectiveMatrix(30.0f, 1280.0f/720.0f, 0.1f, 100.0f)));
+  this->camera->setProjectionMatrix(matrix);
+  //position camera (this should not go here)
+  this->camera->getPosition()->setZ(12.0f);
+  this->ambientLight = std::shared_ptr<Light>(new Light());
+  this->ambientLight->getColor()->setRGBA(0.01f,0.01f,0.01f);
+  this->usesShadows = false;
+  this->PCFShadows = true;
+  this->maxLightIntensity = 0.0f;
 
-	}
+}
 
-	Scene::Scene(const Scene& scene){
-		this->camera = scene.camera;
-		this->ambientLight = scene.ambientLight;
-		this->objects = scene.objects;
-		this->addedObjects = scene.addedObjects;
-		this->removedObjects = scene.removedObjects;
-		this->directionalLights = scene.directionalLights;
-		this->pointLights = scene.pointLights;
-		this->maxLightIntensity = scene.maxLightIntensity;
-		this->usesShadows = scene.usesShadows;
-		this->PCFShadows = scene.PCFShadows;
-	}
+Scene::Scene(const Scene& scene){
+  this->camera = scene.camera;
+  this->ambientLight = scene.ambientLight;
+  this->objects = scene.objects;
+  this->addedObjects = scene.addedObjects;
+  this->removedObjects = scene.removedObjects;
+  this->directionalLights = scene.directionalLights;
+  this->pointLights = scene.pointLights;
+  this->maxLightIntensity = scene.maxLightIntensity;
+  this->usesShadows = scene.usesShadows;
+  this->PCFShadows = scene.PCFShadows;
+}
 
-	Scene::Scene(Scene&& scene){
-		this->camera = move(scene.camera);
-		this->ambientLight = move(scene.ambientLight);
-		this->objects = move(scene.objects);
-		this->addedObjects = move(scene.addedObjects);
-		this->removedObjects = move(scene.removedObjects);
-		this->directionalLights = move(scene.directionalLights);
-		this->pointLights = move(scene.pointLights);
-		this->maxLightIntensity = scene.maxLightIntensity;
-		this->usesShadows = scene.usesShadows;
-		this->PCFShadows = scene.PCFShadows;
-	}
+Scene::Scene(Scene&& scene){
+  this->camera = std::move(scene.camera);
+  this->ambientLight = std::move(scene.ambientLight);
+  this->objects = std::move(scene.objects);
+  this->addedObjects = std::move(scene.addedObjects);
+  this->removedObjects = std::move(scene.removedObjects);
+  this->directionalLights = std::move(scene.directionalLights);
+  this->pointLights = std::move(scene.pointLights);
+  this->maxLightIntensity = scene.maxLightIntensity;
+  this->usesShadows = scene.usesShadows;
+  this->PCFShadows = scene.PCFShadows;
+}
 
-	Scene& Scene::operator=(const Scene& scene){
-		this->camera = scene.camera;
-		this->ambientLight = scene.ambientLight;
-		this->objects = scene.objects;
-		this->addedObjects = scene.addedObjects;
-		this->removedObjects = scene.removedObjects;
-		this->directionalLights = scene.directionalLights;
-		this->pointLights = scene.pointLights;
-		this->maxLightIntensity = scene.maxLightIntensity;
-		this->usesShadows = scene.usesShadows;
-		this->PCFShadows = scene.PCFShadows;
-		return *this;
-	}
+Scene& Scene::operator=(const Scene& scene){
+  this->camera = scene.camera;
+  this->ambientLight = scene.ambientLight;
+  this->objects = scene.objects;
+  this->addedObjects = scene.addedObjects;
+  this->removedObjects = scene.removedObjects;
+  this->directionalLights = scene.directionalLights;
+  this->pointLights = scene.pointLights;
+  this->maxLightIntensity = scene.maxLightIntensity;
+  this->usesShadows = scene.usesShadows;
+  this->PCFShadows = scene.PCFShadows;
+  return *this;
+}
 
-	Scene& Scene::operator=(Scene&& scene){
-		this->camera = move(scene.camera);
-		this->ambientLight = move(scene.ambientLight);
-		this->objects = move(scene.objects);
-		this->addedObjects = move(scene.addedObjects);
-		this->removedObjects = move(scene.removedObjects);
-		this->directionalLights = move(scene.directionalLights);
-		this->pointLights = move(scene.pointLights);
-		this->maxLightIntensity = scene.maxLightIntensity;
-		this->usesShadows = scene.usesShadows;
-		this->PCFShadows = scene.PCFShadows;
-		return *this;
-	}
+Scene& Scene::operator=(Scene&& scene){
+  this->camera = std::move(scene.camera);
+  this->ambientLight = std::move(scene.ambientLight);
+  this->objects = std::move(scene.objects);
+  this->addedObjects = std::move(scene.addedObjects);
+  this->removedObjects = std::move(scene.removedObjects);
+  this->directionalLights = std::move(scene.directionalLights);
+  this->pointLights = std::move(scene.pointLights);
+  this->maxLightIntensity = scene.maxLightIntensity;
+  this->usesShadows = scene.usesShadows;
+  this->PCFShadows = scene.PCFShadows;
+  return *this;
+}
 
-	const vector<shared_ptr<Object3D>>& Scene::getObjects()const{
-		return this->objects;
-	}
+const vector<std::shared_ptr<Object3D>>& Scene::getObjects()const{
+  return this->objects;
+}
 
-	Scene& Scene::addObject(shared_ptr<Object3D> object){
-		if(find(this->objects.begin(),this->objects.end(), object) == this->objects.end()){
-			this->objects.push_back(object);
-		}
-		return *this;
-	}
+Scene& Scene::addObject(std::shared_ptr<Object3D> object){
+  if(find(this->objects.begin(),this->objects.end(), object) == this->objects.end()){
+    this->objects.push_back(object);
+  }
+  return *this;
+}
 
-	Scene& Scene::removeObject(shared_ptr<Object3D> object){
-		auto it = find(this->objects.begin(),this->objects.end(), object);
-		if(it != this->objects.end()){
-			this->objects.erase(it);
-		}
-		return *this;
-	}
+Scene& Scene::removeObject(std::shared_ptr<Object3D> object){
+  auto it = find(this->objects.begin(),this->objects.end(), object);
+  if(it != this->objects.end()){
+    this->objects.erase(it);
+  }
+  return *this;
+}
 
-	shared_ptr<Light> Scene::getAmbientLight()const{
-		return this->ambientLight;
-	}
+std::shared_ptr<Light> Scene::getAmbientLight()const{
+  return this->ambientLight;
+}
 
-	Scene& Scene::setAmbientLight(shared_ptr<Light> ambientLight){
-		this->ambientLight = ambientLight;
-		return *this;
-	}
+Scene& Scene::setAmbientLight(std::shared_ptr<Light> ambientLight){
+  this->ambientLight = ambientLight;
+  return *this;
+}
 
-	shared_ptr<Camera> Scene::getCamera()const{
-		return this->camera;
-	}
+std::shared_ptr<Camera> Scene::getCamera()const{
+  return this->camera;
+}
 
-	Scene& Scene::setCamera(shared_ptr<Camera> camera){
-		this->camera = camera;
-		return *this;
-	}
+Scene& Scene::setCamera(std::shared_ptr<Camera> camera){
+  this->camera = camera;
+  return *this;
+}
 
 
-	const vector<shared_ptr<DirectionalLight>>& Scene::getDirectionalLights()const{
-		return this->directionalLights;
-	}
+const vector<std::shared_ptr<DirectionalLight>>& Scene::getDirectionalLights()const{
+  return this->directionalLights;
+}
 
-	const vector<shared_ptr<PointLight>>& Scene::getPointLights()const{
-		return this->pointLights;
-	}
+const vector<std::shared_ptr<PointLight>>& Scene::getPointLights()const{
+  return this->pointLights;
+}
 
-	Scene& Scene::addPointLight(shared_ptr<PointLight> pointLight){
-		this->pointLights.push_back(pointLight);
-		return *this;
-	}
+Scene& Scene::addPointLight(std::shared_ptr<PointLight> pointLight){
+  this->pointLights.push_back(pointLight);
+  return *this;
+}
 
-	Scene& Scene::addDirectionalLight(shared_ptr<DirectionalLight> directionalLight){
-		this->directionalLights.push_back(directionalLight);
-		return *this;
-	}
+Scene& Scene::addDirectionalLight(std::shared_ptr<DirectionalLight> directionalLight){
+  this->directionalLights.push_back(directionalLight);
+  return *this;
+}
 
-	float Scene::getMaxLightIntensity()const{
-		return this->maxLightIntensity;
-	}
+float Scene::getMaxLightIntensity()const{
+  return this->maxLightIntensity;
+}
 
-	bool Scene::getUsesShadows()const{
-		return this->usesShadows;
-	}
+bool Scene::getUsesShadows()const{
+  return this->usesShadows;
+}
 
-	bool Scene::getPCFShadows()const{
-		return this->PCFShadows;
-	}
+bool Scene::getPCFShadows()const{
+  return this->PCFShadows;
+}
 
-	Scene& Scene::setMaxLightIntensity(float maxLightIntensity){
-		this->maxLightIntensity = maxLightIntensity;
-		return *this;
-	}
+Scene& Scene::setMaxLightIntensity(float maxLightIntensity){
+  this->maxLightIntensity = maxLightIntensity;
+  return *this;
+}
 
-	Scene& Scene::setUsesShadows(bool usesShadows){
-		this->usesShadows = usesShadows;
-		return *this;
-	}
+Scene& Scene::setUsesShadows(bool usesShadows){
+  this->usesShadows = usesShadows;
+  return *this;
+}
 
-	Scene& Scene::setPCFShadows(bool PCFShadows){
-		this->PCFShadows = PCFShadows;
-		return *this;
-	}
+Scene& Scene::setPCFShadows(bool PCFShadows){
+  this->PCFShadows = PCFShadows;
+  return *this;
+}
 
 }
