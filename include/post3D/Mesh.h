a17c1646467176722298c212504409b1764f9492
fernandomv3 <fernandomv3@gmail.com>
2015-09-04 15:01:49 -0500

    Mesh class indentation and namespacing fix

--- include/post3D/Mesh.h
+++ include/post3D/Mesh.h
@@ -3,36 +3,36 @@
 
 #include "post3D/Object3D.h"
 #include "post3D/Geometry.h"
-//#include "post3D/Material.h"
-#include "memory"
+#include <memory>
 
 namespace material{
 class Material;
 }
-using namespace std;
+
 using namespace material;
+
 namespace object3D{
-	class Mesh : public Object3D{
-	private:
-		shared_ptr<Geometry> geometry;
-		shared_ptr<Material> material;
-		bool castShadows;
-	public:
-		Mesh();
-		Mesh(shared_ptr<Geometry> geometry);
-		Mesh(shared_ptr<Geometry> geometry, shared_ptr<Material> material);
-		Mesh(const Mesh& mesh);
-		Mesh(Mesh&& mesh);
-		Mesh& operator=(const Mesh& mesh);
-		Mesh& operator=(Mesh&& mesh);
-		~Mesh()=default;
-		shared_ptr<Geometry> getGeometry()const;
-		shared_ptr<Material> getMaterial()const;
-		Mesh& setGeometry(shared_ptr<Geometry> geometry);
-		Mesh& setMaterial(shared_ptr<Material> material);
-		bool getCastShadows()const;
-		Mesh& setCastShadows(bool castShadows);
-	};
+class Mesh : public Object3D{
+private:
+  std::shared_ptr<Geometry> geometry;
+  std::shared_ptr<Material> material;
+  bool castShadows;
+public:
+  Mesh();
+  Mesh(std::shared_ptr<Geometry> geometry);
+  Mesh(std::shared_ptr<Geometry> geometry, std::shared_ptr<Material> material);
+  Mesh(const Mesh& mesh);
+  Mesh(Mesh&& mesh);
+  Mesh& operator=(const Mesh& mesh);
+  Mesh& operator=(Mesh&& mesh);
+  ~Mesh()=default;
+  std::shared_ptr<Geometry> getGeometry()const;
+  std::shared_ptr<Material> getMaterial()const;
+  Mesh& setGeometry(std::shared_ptr<Geometry> geometry);
+  Mesh& setMaterial(std::shared_ptr<Material> material);
+  bool getCastShadows()const;
+  Mesh& setCastShadows(bool castShadows);
+};
 }
 
 
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -2,82 +2,82 @@
 
 namespace object3D{
 
-	Mesh::Mesh():Object3D(){
-		this->geometry = shared_ptr<Geometry>();
-		this->material = shared_ptr<Material>();
-		this->castShadows = true;
-	}
+Mesh::Mesh():Object3D(){
+  this->geometry = std::shared_ptr<Geometry>();
+  this->material = std::shared_ptr<Material>();
+  this->castShadows = true;
+}
 
-	Mesh::Mesh(shared_ptr<Geometry> geometry):Object3D(){
-		this->geometry = geometry;
-		this->material = shared_ptr<Material>();
-		this->castShadows = true;
-	}
+Mesh::Mesh(std::shared_ptr<Geometry> geometry):Object3D(){
+  this->geometry = geometry;
+  this->material = std::shared_ptr<Material>();
+  this->castShadows = true;
+}
 
-	Mesh::Mesh(shared_ptr<Geometry> geometry, shared_ptr<Material> material):Object3D(){
-		this->geometry = geometry;
-		this->material = material;
-		this->castShadows = true;
-	}
+Mesh::Mesh(std::shared_ptr<Geometry> geometry, std::shared_ptr<Material> material):Object3D(){
+  this->geometry = geometry;
+  this->material = material;
+  this->castShadows = true;
+}
 
-	Mesh::Mesh(const Mesh& mesh):Object3D(mesh){
-		this->geometry = mesh.geometry;
-		this->material = mesh.material;
-		this->castShadows = mesh.castShadows;
-	}
+Mesh::Mesh(const Mesh& mesh):Object3D(mesh){
+  this->geometry = mesh.geometry;
+  this->material = mesh.material;
+  this->castShadows = mesh.castShadows;
+}
 
-	Mesh::Mesh(Mesh&& mesh):Object3D(mesh){
-		this->geometry = move(mesh.geometry);
-		this->material = move(mesh.material);
-		this->castShadows = mesh.castShadows;
-	}
+Mesh::Mesh(Mesh&& mesh):Object3D(mesh){
+  this->geometry = std::move(mesh.geometry);
+  this->material = std::move(mesh.material);
+  this->castShadows = mesh.castShadows;
+}
 
-	Mesh& Mesh::operator=(const Mesh& mesh){
-		Object3D::operator=(mesh);
-		this->geometry = mesh.geometry;
-		this->material = mesh.material;
-		this->castShadows = mesh.castShadows;
-		return *this;
-	}
+Mesh& Mesh::operator=(const Mesh& mesh){
+  Object3D::operator=(mesh);
+  this->geometry = mesh.geometry;
+  this->material = mesh.material;
+  this->castShadows = mesh.castShadows;
+  return *this;
+}
 
-	Mesh& Mesh::operator=(Mesh&& mesh){
-		Object3D::operator=(move(mesh));
-		this->geometry = move(mesh.geometry);
-		this->material = move(mesh.material);
-		this->castShadows = mesh.castShadows;
-		return *this;
-	}
+Mesh& Mesh::operator=(Mesh&& mesh){
+  Object3D::operator=(std::move(mesh));
+  this->geometry = std::move(mesh.geometry);
+  this->material = std::move(mesh.material);
+  this->castShadows = mesh.castShadows;
+  return *this;
+}
 
-/*	Mesh::~Mesh(){
+/*  Mesh::~Mesh(){
 
-	}*/
+}*/
 
-	shared_ptr<Geometry> Mesh::getGeometry()const{
-		return this->geometry;
-	}
+std::shared_ptr<Geometry> Mesh::getGeometry()const{
+  return this->geometry;
+}
 
-	shared_ptr<Material> Mesh::getMaterial()const{
-		return this->material;
-	}
+std::shared_ptr<Material> Mesh::getMaterial()const{
+  return this->material;
+}
 
-	Mesh& Mesh::setGeometry(shared_ptr<Geometry> geometry){
-		this->geometry = geometry;
-		return *this;
-	}
+Mesh& Mesh::setGeometry(std::shared_ptr<Geometry> geometry){
+  this->geometry = geometry;
+  return *this;
+}
 
-	Mesh& Mesh::setMaterial(shared_ptr<Material> material){
-		this->material = material;
-		return *this;
-	}
+Mesh& Mesh::setMaterial(std::shared_ptr<Material> material){
+  this->material = material;
+  return *this;
+}
 
-	bool Mesh::getCastShadows()const{
-		return this->castShadows;
-	}
+bool Mesh::getCastShadows()const{
+  return this->castShadows;
+}
 
-	Mesh& Mesh::setCastShadows(bool castShadows){
-		this->castShadows = castShadows;
-		return *this;
-	}
+Mesh& Mesh::setCastShadows(bool castShadows){
+  this->castShadows = castShadows;
+  return *this;
+}
 
 
 }
