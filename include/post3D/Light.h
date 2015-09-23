6ae47fa598a715bfb0d5162591597711661d28df
fernandomv3 <fernandomv3@gmail.com>
2015-09-23 17:23:15 -0500

    Light indentation and namespacing fix

--- include/post3D/Light.h
+++ include/post3D/Light.h
@@ -4,22 +4,21 @@
 #include "post3D/Color.h"
 #include <memory>
 
-using namespace std;
 using namespace object3D;
 
 namespace light{
 class Light : public Object3D {
 private:
-	shared_ptr<Color> color;
+  std::shared_ptr<Color> color;
 public:
-	Light();
-	Light(const Light& light);
-	Light(Light&& light);
-	Light& operator=(const Light& light);
-	Light& operator=(Light&& light);
-	virtual ~Light()=default;
-	shared_ptr<Color> getColor() const;
-	Light& setColor(shared_ptr<Color> color);
+  Light();
+  Light(const Light& light);
+  Light(Light&& light);
+  Light& operator=(const Light& light);
+  Light& operator=(Light&& light);
+  virtual ~Light()=default;
+  std::shared_ptr<Color> getColor() const;
+  Light& setColor(std::shared_ptr<Color> color);
 };
 }
 #endif
--- src/Light.cpp
+++ src/Light.cpp
@@ -1,40 +1,39 @@
 ﻿#include "post3D/Light.h"
-using namespace std;
 using namespace math3D;
 
 namespace light{
 
 Light::Light():Object3D(){
-	this->color = shared_ptr<Color>(new Color());
+  this->color = std::shared_ptr<Color>(new Color());
 }
 
 Light::Light(const Light& light):Object3D(light){
-	this->color = light.color;
+  this->color = light.color;
 }
 
 Light::Light(Light&& light):Object3D(light){
-	this->color = move(light.color);
+  this->color = std::move(light.color);
 }
 
 Light& Light::operator=(const Light& light){
-	Object3D::operator=(light);
-	this->color = light.color;
-	return *this;
+  Object3D::operator=(light);
+  this->color = light.color;
+  return *this;
 }
 
 Light& Light::operator=(Light&& light){
-	Object3D::operator=(move(light));
-	this->color = move(light.color);
-	return *this;
+  Object3D::operator=(std::move(light));
+  this->color = std::move(light.color);
+  return *this;
 }
 
-shared_ptr<Color> Light::getColor()const{
-	return this->color;
+std::shared_ptr<Color> Light::getColor()const{
+  return this->color;
 }
 
-Light& Light::setColor(shared_ptr<Color> color){
-	this->color = color;
-	return *this;
+Light& Light::setColor(std::shared_ptr<Color> color){
+  this->color = color;
+  return *this;
 }
 
 }
